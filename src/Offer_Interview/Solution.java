ad1ff682b0a9ff1f82846bd9d7e5f39f3f8d16fc
eKingsDu <153263008@qq.com>
2018-08-03 11:57:43 +0800

    二维数组中的查找

--- src/Offer_Interview/Solution.java
+++ src/Offer_Interview/Solution.java
@@ -11,4 +11,52 @@ package Offer_Interview;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        Integer[][] array = {{1,2,3},{4,5,6},{7,8,9}};
+        if(Solution.Find(array,-1)!=null){
+            System.out.println("true");
+        }else {
+            System.out.println("flase");
+        }
+    }
+
+    /***
+     * 1 发现可使用的规律  -- 左上 右下
+     * 2 确定变化的是什么  -- Col Row
+     * 3 循环的方式  -- 1 范围；2 怎么变化；3 如何实现
+     */
+    public static Integer Find(Integer[][] array,Integer target){
+        Integer col = array.length-1;
+        Integer row = 0;
+
+        while(col >=0 && row<=array[0].length-1){
+            if(target == array[col][row]){
+                return target;
+            }else if(target < array[col][row]){
+                col--;
+            }else if(target > array[col][row]){
+                row++;
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
