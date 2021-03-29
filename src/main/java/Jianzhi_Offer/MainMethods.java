9f7659b3190278c8e11c11e33d92c1b327a51062
lenovo <153263008@qq.com>
2021-03-29 21:09:23 +0800

    剑指offer-二维数组中的查找，位置查找法

--- src/main/java/Jianzhi_Offer/MainMethods.java
+++ src/main/java/Jianzhi_Offer/MainMethods.java
@@ -10,15 +10,26 @@ public class MainMethods {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-
-        int target = 111;
-        int [][] arrayTar = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
         Solution sl = new Solution();
-        System.out.println(sl.Find(target,arrayTar));
 
+        //生成随机数组
+        int rows = 31500,cols = 31400;
+        int[][] arrayListGet = sl.ArrayList_get(rows, cols);
+
+        //遍历法
+//        int target = 111;
+////        int [][] arrayTar = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+//        System.out.println(sl.Find_1(target,arrayListGet));
+
+        //位置法
+        int target = 111;
+//        int [][] arrayTar = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+        System.out.println(sl.Find_2(target,arrayListGet));
 
         long endTime = System.currentTimeMillis();
         System.out.println("运行时间："+new BigDecimal(endTime-startTime).divide(new BigDecimal(1000)).doubleValue());
     }
+
+
 
 }
--- src/main/java/Jianzhi_Offer/Solution.java
+++ src/main/java/Jianzhi_Offer/Solution.java
@@ -1,6 +1,6 @@
 package Jianzhi_Offer;
 
-import java.math.BigDecimal;
+import java.util.Random;
 
 /***
  *
@@ -11,8 +11,13 @@ import java.math.BigDecimal;
 
 public class Solution {
 
-
-    public boolean Find(int target,int [][] array){
+    /**
+     * 数组遍历法则
+     * @param target
+     * @param array
+     * @return
+     */
+    public boolean Find_1(int target,int [][] array){
         boolean flag = false;
 
         for (int i = 0; i < array.length; i++) {
@@ -24,11 +29,68 @@ public class Solution {
             }
         }
 
-
         return flag;
     }
 
 
+    /**
+     *
+     * 位置判断法
+     *
+     * @param target
+     * @param array
+     * @return
+     */
+    public boolean Find_2(int target,int [][] array){
+        int rows = array.length;
+        int cols = array[0].length;
+        int i=rows-1,j=0;
 
+        while(i>=0 && j<cols){
+            if(array[i][j]>target)
+                i--;
+            else if(array[i][j]<target)
+                j++;
+            else
+                return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 生成指定行列的数组
+     * @param cols
+     * @param rows
+     * @return
+     */
+    public int [][] ArrayList_get(int rows,int cols){
+        int [][] array_a = new int[rows][cols];
+        Random ran = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array_a[i][j] = ran.nextInt(100);
+            }
+        }
+
+//        for (int i = 0; i < array_a.length; i++) {
+//            for (int j = 0; j < array_a[i].length; j++) {
+//                System.out.print(array_a[i][j]+"--");
+//            }
+//            System.out.println();
+//        }
+
+        return array_a;
+    }
 
 }
+
+
+/**
+ *
+ * 结论：
+ * （1）遍历法是根据数组左下角和右上角的数字间的规律进行比较的，要注意循环的条件控制以及数组的边界控制。
+ * （2）遍历法和位置法都是逐个在比较，两者的效率相差不大。
+ * （3）Random随机函数里填充的数字100，生成的结果是0~99的数字。
+ */
