d751f6842143b61758501a5c5aab6faca24589ed
lenovo <153263008@qq.com>
2021-04-14 17:20:53 +0800

    剑指offer-- Josephu 问题

--- src/main/java/Jianzhi_Offer/JumpFloorII.java
+++ src/main/java/Jianzhi_Offer/JumpFloorII.java
@@ -6,13 +6,13 @@ package Jianzhi_Offer;
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  *
  */
-public class jumpFloorII
+public class JumpFloorII
 {
 
 
     public static void main(String[] args)
     {
-        jumpFloorII jumpFloorII = new jumpFloorII();
+        JumpFloorII jumpFloorII = new JumpFloorII();
         System.out.println(jumpFloorII.jumpFloorIIJump(3));
     }
 
